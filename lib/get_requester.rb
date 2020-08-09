14705855e4ae41de8bc2ea8eda42f9aa21221d0a
Raveen K Sharma <raveen08@gmail.com>
2020-08-09 20:08:27 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,5 +7,11 @@ class GetRequester
   
   def get_response_body
     url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
+  def parse_json
     
     
