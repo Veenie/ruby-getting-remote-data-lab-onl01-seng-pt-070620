67aba33be0ee93aa1a28eae10a7840b2d62bf912
Raveen K Sharma <raveen08@gmail.com>
2020-08-09 20:07:15 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,9 +1,11 @@
 # Write your code here
-
-
+require 'net/http'
+require 'open-uri'
+require 'json'
 
 class GetRequester
   
   def get_response_body
     url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    
     
